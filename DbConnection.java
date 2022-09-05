3256272be16a31d478fe7c4d79f0236e78c4aed0
FrenDo0 <74112298+FrenDo0@users.noreply.github.com>
2022-09-05 23:36:49 +0300

    Fixed bug with connection

--- DbConnection.java
+++ DbConnection.java
@@ -1,48 +1,25 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.util.ResourceBundle;
 
 public class DbConnection {
 
     private static Connection con;
     static
     {
-        String url = "";
-        String username = "";
-        String pass = "";
-        url = getCredentials()[0];
-        username = getCredentials()[1];
-        pass = getCredentials()[2];
+        ResourceBundle rb = ResourceBundle.getBundle("connection");
+        String url = rb.getString("url");
+        String username = rb.getString("username");
+        String password = rb.getString("password");
         try
         {
-            con = DriverManager.getConnection(url, username, pass);
+            con = DriverManager.getConnection(url,username,password);
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
-
     public static Connection getConnection(){
         return con;
-    }
-
-    private static String[] getCredentials(){
-        String[] input = new String[0];
-        try
-        {
-            File file = new File("credentials.txt");
-            Scanner sc = new Scanner(file);
-            while(sc.hasNextLine()){
-                String data = sc.nextLine();
-                input = data.split("->",3);
-            }
-            sc.close();
-
-        }catch (FileNotFoundException e){
-            System.out.println("File not found");
-        }
-        return input;
     }
 }
